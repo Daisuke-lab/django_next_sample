f2a8286793afff92db665dc4055b9c195e55f676
t.uzuhara <t.uzuhara@segment.co.jp>
2022-02-10 12:09:50 +0900

    prescoDBに接続する処理をすべてコメントアウト

--- backend/backend_module/common.py
+++ backend/backend_module/common.py
@@ -9,23 +9,23 @@ import datetime
 
 class Common:
     def __init__(self):
-        self.conn_presco = self.connect_presco_DB()
+        # self.conn_presco = self.connect_presco_DB()
         self.conn_presco_copy = self.conncet_presco_copy_DB()
         self.BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         self.ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
-    def connect_presco_DB(self):
-        """DB接続
+    # def connect_presco_DB(self):
+    #     """DB接続
 
-        prescoaiが格納されているDBに接続し、コントローラーを返す
+    #     prescoaiが格納されているDBに接続し、コントローラーを返す
 
-        """
-        host = "dotai-production.cf6ivuffba0e.ap-northeast-1.rds.amazonaws.com"
+    #     """
+    #     host = "dotai-production.cf6ivuffba0e.ap-northeast-1.rds.amazonaws.com"
 
-        conn = mysql.connector.connect(
-            host=host, port=3306, user="t.uzuhara", password="Tuzu0101", database="prescoai"
-        )
-        return conn
+    #     conn = mysql.connector.connect(
+    #         host=host, port=3306, user="t.uzuhara", password="Tuzu0101", database="prescoai"
+    #     )
+    #     return conn
 
     def conncet_presco_copy_DB(self):
         """DB接続
--- backend/backend_module/update_domain.py
+++ backend/backend_module/update_domain.py
@@ -15,72 +15,72 @@ from googleapiclient.discovery import build
 from domains.models import Domain, Url, Trademark
 
 class UpdateDomain(Common):
-    def get_dotai_domains(self):
-        all_domains = []
-        query1 = (
-            f"SELECT alliance.ID FROM program_url INNER JOIN alliance "
-            "ON program_url.id = alliance.PROGRAM_URL_ID "
-            f"WHERE program_url.MERCHANT_ID = {self.client_presco_id}"
-        )
+    # def get_dotai_domains(self):
+    #     all_domains = []
+    #     query1 = (
+    #         f"SELECT alliance.ID FROM program_url INNER JOIN alliance "
+    #         "ON program_url.id = alliance.PROGRAM_URL_ID "
+    #         f"WHERE program_url.MERCHANT_ID = {self.client_presco_id}"
+    #     )
 
-        alliance_ids = self.commit_query(conn=self.conn_presco, query=query1, select=True)
-        partner_site_page_ids = []
-        for value in alliance_ids:
-            alliance_id = value["ID"]
-            query2 = (
-                "SELECT PARTNER_SITE_PAGE_ID, end_date FROM alliance INNER JOIN alliance_history ON "
-                f"alliance.id = alliance_history.alliance_id WHERE alliance_id = {alliance_id};"
-            )
-            end_check_result = self.commit_query(conn=self.conn_presco, query=query2, select=True)
-            end_date = end_check_result[0]["end_date"]
-            if end_date is None:
-                partner_site_page_ids.append(end_check_result[0]["PARTNER_SITE_PAGE_ID"])
-            elif end_date > datetime.date.today():
-                partner_site_page_ids.append(end_check_result[0]["PARTNER_SITE_PAGE_ID"])
+    #     alliance_ids = self.commit_query(conn=self.conn_presco, query=query1, select=True)
+    #     partner_site_page_ids = []
+    #     for value in alliance_ids:
+    #         alliance_id = value["ID"]
+    #         query2 = (
+    #             "SELECT PARTNER_SITE_PAGE_ID, end_date FROM alliance INNER JOIN alliance_history ON "
+    #             f"alliance.id = alliance_history.alliance_id WHERE alliance_id = {alliance_id};"
+    #         )
+    #         end_check_result = self.commit_query(conn=self.conn_presco, query=query2, select=True)
+    #         end_date = end_check_result[0]["end_date"]
+    #         if end_date is None:
+    #             partner_site_page_ids.append(end_check_result[0]["PARTNER_SITE_PAGE_ID"])
+    #         elif end_date > datetime.date.today():
+    #             partner_site_page_ids.append(end_check_result[0]["PARTNER_SITE_PAGE_ID"])
 
-        partner_site_page_ids = set(partner_site_page_ids)
-        for partner_site_page_id in partner_site_page_ids:
-            query3 = f"SELECT PARTNER_SITE_URL_OTHER FROM partner_site_page WHERE id = {partner_site_page_id}"
-            partner_site_result = self.commit_query(
-                conn=self.conn_presco, query=query3, select=True
-            )
-            all_domains.append(partner_site_result[0]["PARTNER_SITE_URL_OTHER"])
-        all_domains = list(set(all_domains))
-        return all_domains
+    #     partner_site_page_ids = set(partner_site_page_ids)
+    #     for partner_site_page_id in partner_site_page_ids:
+    #         query3 = f"SELECT PARTNER_SITE_URL_OTHER FROM partner_site_page WHERE id = {partner_site_page_id}"
+    #         partner_site_result = self.commit_query(
+    #             conn=self.conn_presco, query=query3, select=True
+    #         )
+    #         all_domains.append(partner_site_result[0]["PARTNER_SITE_URL_OTHER"])
+    #     all_domains = list(set(all_domains))
+    #     return all_domains
 
-    def get_referrer_domain(self):
-        domains = []
-        update_domains = []
-        query1 = (
-            f"SELECT DISTINCT referrer,pg_id FROM click_log "
-            f"WHERE merchant_id = {self.client_presco_id} && referrer != ''"
-        )
-        click_logs = self.commit_query(conn=self.conn_presco, query=query1, select=True)
-        for click_log in click_logs:
-            referrer = click_log["referrer"]
-            domain = re.match(r"(https?://[^/]+/)", referrer)
-            if domain is not None:
-                if domain.group(0) not in domains and ".safeframe" not in domain.group(0):
-                    domains.append(domain.group(0))
-        for domain in domains:
-            query2 = f"SELECT id FROM partner_site_page WHERE partner_site_url_other = '{domain}'"
-            partner_site_pages = self.commit_query(
-                conn=self.conn_presco, query=query2, select=True
-            )
-            if partner_site_pages != []:
-                partner_site_page = partner_site_pages[0]
-                query3 = (
-                    "SELECT end_date FROM summary_log INNER JOIN alliance_history ON "
-                    "summary_log.alliance_id = alliance_history.alliance_id "
-                    f"WHERE summary_log.partner_site_page_id = {partner_site_page['id']}"
-                )
-                end_dates = self.commit_query(conn=self.conn_presco, query=query3, select=True)
-                end_dates = [i["end_date"] for i in end_dates]
-                if end_dates.count(None) == len(end_dates):
-                    update_domains.append(str(domain))
-            else:
-                update_domains.append(str(domain))
-        return update_domains
+    # def get_referrer_domain(self):
+    #     domains = []
+    #     update_domains = []
+    #     query1 = (
+    #         f"SELECT DISTINCT referrer,pg_id FROM click_log "
+    #         f"WHERE merchant_id = {self.client_presco_id} && referrer != ''"
+    #     )
+    #     click_logs = self.commit_query(conn=self.conn_presco, query=query1, select=True)
+    #     for click_log in click_logs:
+    #         referrer = click_log["referrer"]
+    #         domain = re.match(r"(https?://[^/]+/)", referrer)
+    #         if domain is not None:
+    #             if domain.group(0) not in domains and ".safeframe" not in domain.group(0):
+    #                 domains.append(domain.group(0))
+    #     for domain in domains:
+    #         query2 = f"SELECT id FROM partner_site_page WHERE partner_site_url_other = '{domain}'"
+    #         partner_site_pages = self.commit_query(
+    #             conn=self.conn_presco, query=query2, select=True
+    #         )
+    #         if partner_site_pages != []:
+    #             partner_site_page = partner_site_pages[0]
+    #             query3 = (
+    #                 "SELECT end_date FROM summary_log INNER JOIN alliance_history ON "
+    #                 "summary_log.alliance_id = alliance_history.alliance_id "
+    #                 f"WHERE summary_log.partner_site_page_id = {partner_site_page['id']}"
+    #             )
+    #             end_dates = self.commit_query(conn=self.conn_presco, query=query3, select=True)
+    #             end_dates = [i["end_date"] for i in end_dates]
+    #             if end_dates.count(None) == len(end_dates):
+    #                 update_domains.append(str(domain))
+    #         else:
+    #             update_domains.append(str(domain))
+    #     return update_domains
 
     def get_presco_copy_domains(self, product_id):
         query = f"SELECT domain FROM presco_db_copy WHERE pg_id = '{product_id}'"
