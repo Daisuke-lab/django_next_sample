fe3a4fe8bd5bb11fa84a99bebe59ca651b57be65
d.kikuchi <d.kikuchi@segment.co.jp>
2022-02-10 13:40:33 +0900

    webhook-test

--- backend/config/views.py
+++ backend/config/views.py
@@ -10,7 +10,7 @@ def celery_test(request):
 
 def home(request):
     #health check用
-    return HttpResponse("this is medicine robo backend home1")
+    return HttpResponse("this is medicine robo backend home")
 
 
 def check_start(request):
