136a749af3ce6004f92a24599e809dc52cd08d6d
t.uzuhara <t.uzuhara@segment.co.jp>
2022-02-10 19:01:26 +0900

    環境変数に関するエラー解消

--- backend/backend_module/config.py
+++ backend/backend_module/config.py
@@ -1,14 +1,12 @@
 import environ
 import os
+from dotenv import load_dotenv
 
 environment = os.environ.get('ENVIRONMENT', 'development')
-
+load_dotenv()
 if environment == "development":
-    env = environ.Env()
-    env.read_env('.env')
-
-    GOOGLE_API_KEY = env("GOOGLE_API_KEY")
-    CUSTOM_SEARCH_ENGINE_KEY = env("CUSTOM_SEARCH_ENGINE_KEY")
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+    CUSTOM_SEARCH_ENGINE_KEY = os.getenv("CUSTOM_SEARCH_ENGINE_KEY")
 elif environment == "production":
     GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
     CUSTOM_SEARCH_ENGINE_KEY = os.environ.get("CUSTOM_SEARCH_ENGINE_KEY")
