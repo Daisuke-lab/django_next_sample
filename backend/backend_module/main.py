5b4f609655b96e68db724ba8bfe6a58238fc7a06
t.uzuhara <t.uzuhara@segment.co.jp>
2022-02-11 15:51:04 +0900

    ローカル環境でのテスト実行に基づく修正

--- backend/backend_module/main.py
+++ backend/backend_module/main.py
@@ -5,5 +5,5 @@ from ng_check import NgCheck
 def main(product_id_list):
     for product_id in product_id_list:
         domains = UpdateDomain().job(product_id=product_id)
-        UpdateTargetUrl().job(domains=domains)
+        UpdateTargetUrl().job(domains=domains, product_id=product_id)
         NgCheck().job(domains=domains)
